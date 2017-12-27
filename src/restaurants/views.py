148cc86746b03886778b19dafd35354b83e6e2a7
Max Stolovich <maxstolovich@gmail.com>
2017-12-27 19:05:09 +0200

    Restaurant profile page created

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.db.models import Q
 
 from .models import Restaurant
 
@@ -14,4 +15,32 @@ def restaurant_list_view(request):
             'object_list': queryset
             }
     return render(request, template_name, context)
+ 
+    
+class RestaurantListView(ListView):
+    template_name = 'restaurants/restaurants_list.html'
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        if slug:
+            queryset = Restaurant.objects.filter(
+                    Q(category__iexact=slug) |
+                    Q(category__icontains=slug))
+        else:
+            queryset = Restaurant.objects.all()
+        return queryset
+    
 
+
+class RestaurantDetailView(DetailView):
+    queryset = Restaurant.objects.all()
+    
+    def get_context_data(self, *args, **kwargs):
+        print(self.kwargs)
+        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+    
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(Restaurant, id=rest_id)
+        return obj
