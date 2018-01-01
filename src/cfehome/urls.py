3fe8976e4bcc3a92518256691f52f5862c9356e6
Max Stolovich <maxstolovich@gmail.com>
2018-01-01 11:39:18 +0200

    Owner field added to the model. Associate user to form data in FBV.

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 from restaurants.views import (
+                               restaurant_createview,
                                RestaurantListView,
                                RestaurantDetailView, 
                                RestaurantCreateView
@@ -28,8 +29,9 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^restaurants/$', RestaurantListView.as_view()),
     #url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
-    url(r'^restaurants/create/$', RestaurantCreateView.as_view()),
+    url(r'^restaurants/create/$', restaurant_createview), #RestaurantCreateView.as_view()),
     url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
     url(r'^about/', TemplateView.as_view(template_name='about.html')),
     url(r'^contacts/', TemplateView.as_view(template_name='contacts.html')),
+
 ]
