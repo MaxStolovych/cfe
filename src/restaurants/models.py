c969db710a4ecedf0db9d804c427dc9f6d1fd693
Max Stolovich <maxstolovich@gmail.com>
2018-01-10 08:44:39 +0200

    Search implemented

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.core.urlresolvers import reverse
 
@@ -7,6 +8,25 @@ from .utils import unique_slug_generator
 from .validators import validate_category
 
 User = settings.AUTH_USER_MODEL
+
+class RestaurantQuerySet(models.query.QuerySet):
+    def search(self, query): #Restaurant.objects.all().search(query) #Restaurant.objects.filter(something).search()
+        if query:
+            query = query.strip()
+            return self.filter(
+                Q(name__icontains=query) |
+                Q(location__icontains=query) |
+                Q(category__icontains=query) |
+                Q(item__name__icontains=query) |
+                Q(item__contents__icontains=query)
+                ).distinct()
+
+class RestaurantManager(models.Manager):
+    def get_queryset(self):
+        return RestaurantQuerySet(self.model, using=self._db)
+
+    def search(self, query): #Restaurant.objects.search()
+        return self.get_queryset().filter(name__icontains=query)
 
 class Restaurant(models.Model):
     owner      = models.ForeignKey(User)
@@ -17,7 +37,9 @@ class Restaurant(models.Model):
     timestamp  = models.DateTimeField(auto_now_add=True)
     updated    = models.DateTimeField(auto_now=True)
     slug       = models.SlugField(null=True, blank=True)
-    
+
+    objects = RestaurantManager()
+
     def __str__(self):
         return self.name
 
